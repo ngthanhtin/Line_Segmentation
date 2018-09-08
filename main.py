f94be309564dd0a9845e3c9952f2aa14e732cb1c
ngthanhtin <thanhtinqn0903039105@gmail.com>
2018-09-08 23:32:29 +0700

    main to read image and segment it

---
+++ main.py
@@ -0,0 +1,29 @@
+from LineSegmentation import LineSegmentation
+import cv2
+def line_segment(img, output_path):
+    """
+    img: image needed to segment to lines.
+    output_path: output folder of segmented images. 
+    :return: output image pathes
+    """
+    cv2.imshow("Original", img)
+    cv2.waitKey()
+    # (1) Segment Lines
+    line_segmentation = LineSegmentation(img=img, output_path=output_path)
+    lines = line_segmentation.segment()
+    # (2) Save lines to file
+    output_image_path = line_segmentation.save_lines_to_file(lines)
+
+    return output_image_path
+    
+    
+if __name__ == "__main__":
+    # read image
+    img = cv2.imread("test/Line_1.jpg")
+
+    #do program
+    output_path = "result"
+    r =line_segment(img, output_path)
+    print(r)
+
+
